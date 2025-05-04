a27eb76fb2ca1cfaef6a11f4a3cd819fb2ea4d71
Colin Dunn <colin@ritual.com>
2025-05-04 16:56:01 -0700

    Fix Image Grid

--- components/content/ImageGrid.jsx
+++ components/content/ImageGrid.jsx
@@ -11,7 +11,7 @@ const ImageGrid = ({ type, items }) => {
       {items.map(({ title, description, image }, index) => {
         return (
           <div className={styles.item} key={index}>
-            <img className={styles.image} src={image} />
+            <img className={styles.image} src={image?.url} />
             <div className={styles.content}>
               <span className={styles.title}>{title}</span>
               <div className={styles.description}>
